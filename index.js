ad0a3a9064c230702ea539a57ba468ae2fd8b52e
Henric Trotzig <henric.trotzig@gmail.com>
2018-06-27 07:18:14 +0200

    Extend plugins instead of overriding
    A recent change to the happo.io client lib turned the plugins array into
    an empty array by default. We shouldn't override it, instead we should
    just extend it.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const path = require('path');
-
 const defaultStorybookConfig = require('@storybook/core/dist/server/config/defaults/webpack.config.js');
 const webpack = require('webpack');
 
 module.exports = function happoPluginStorybook({ configDir = '.storybook' } = {}) {
   return {
     customizeWebpackConfig: config => {
-      config.plugins = [
+      config.plugins.push(
         new webpack.DefinePlugin({
           HAPPO_STORYBOOK_CONFIG_FILE: JSON.stringify(
             path.resolve(process.cwd(), configDir, 'config.js'),
           ),
         }),
-      ];
+      );
       const storybookWebpackConfig = require(path.resolve(
         process.cwd(),
         configDir,
