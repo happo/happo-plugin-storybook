b574d6018678aca9ac1a6ea4beafeb3e8dcc54c0
Henric Trotzig <henric.trotzig@gmail.com>
2018-09-18 14:38:00 +0200

    Add ignoredStories config option
    Some stories are hard to render in a happo environment. To opt out,
    users can now add the story name/kind to the `ignoredStories` config
    option.
    
    Also includes a change to make `addons` a local variable, not a global
    one.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const defaultStorybookConfig = require('@storybook/core/dist/server/config/defaults/webpack.config.js');
 const webpack = require('webpack');
 
-module.exports = function happoPluginStorybook({ configDir = '.storybook' } = {}) {
+module.exports = function happoPluginStorybook({ configDir = '.storybook', ignoredStories = [] } = {}) {
   return {
     customizeWebpackConfig: config => {
       config.plugins.push(
@@ -10,6 +10,7 @@ module.exports = function happoPluginStorybook({ configDir = '.storybook' } = {}
           HAPPO_STORYBOOK_CONFIG_FILE: JSON.stringify(
             path.resolve(process.cwd(), configDir, 'config.js'),
           ),
+          HAPPO_STORYBOOK_IGNORED_STORIES: JSON.stringify(ignoredStories),
         }),
       );
       const storybookWebpackConfig = require(path.resolve(
