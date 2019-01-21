75fb3628436db7eb1e87a78606c3ca5e972773e4
Henric Trotzig <henric.trotzig@gmail.com>
2019-01-21 20:48:11 +0100

    Add more targets to .happo.js

--- .happo.js
+++ .happo.js
@@ -7,6 +7,10 @@ const happoPluginStorybook = require('.');
 module.exports = {
   targets: {
     chrome: new RemoteBrowserTarget('chrome', { viewport: '800x600' }),
+    firefox: new RemoteBrowserTarget('firefox', { viewport: '800x600' }),
+    edge: new RemoteBrowserTarget('edge', { viewport: '800x600' }),
+    safari: new RemoteBrowserTarget('safari', { viewport: '800x600' }),
+    ie: new RemoteBrowserTarget('internet explorer', { viewport: '800x600' }),
   },
   plugins: [happoPluginStorybook({ configDir: '.storybook', outputDir: '.happo-out' })],
   stylesheets: [path.resolve(__dirname, 'test.css')],
