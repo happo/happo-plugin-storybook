814959a000e50c87d88f589671a696174d3f9e77
Henric Trotzig <henric.trotzig@gmail.com>
2019-03-13 11:06:14 +0100

    Remove custom file-loader webpack config
    This config seems to be part of storybook's default in v5 and the custom
    one we had was messing with the base config.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,18 +4,6 @@ const result = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
   },
-  module: {
-    rules: [
-      {
-        test: /\.(jpg|png|otf|ttf|woff|woff2|eot|pdf)$/,
-        loader: require.resolve('file-loader'),
-        options: {
-          name: 'static/media/[name].[hash:8].[ext]',
-          publicPath: '/',
-        },
-      },
-    ],
-  },
 };
 
 module.exports = result;
