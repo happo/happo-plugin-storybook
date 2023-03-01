9f4ad72462e148818c028bac66d41940cde3369b
Henric Trotzig <henric.trotzig@gmail.com>
2023-03-01 16:29:59 +0100

    Disable react-webpack5 framework for SB v6 and v5
    This new library was added as part of the v6-v7 migration and is messing
    with older Storybook versions.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,10 @@
+const framework = process.env.DISABLE_REACT_WEBPACK5_FRAMEWORK
+  ? undefined
+  : {
+      name: '@storybook/react-webpack5',
+      options: {},
+    };
+
 module.exports = {
   features: {
     storyStoreV7: process.env.USE_STORYSTORE_V7 !== 'false',
@@ -8,8 +15,5 @@ module.exports = {
     '@storybook/addon-interactions',
     '../preset.js',
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
-  },
+  framework,
 };
