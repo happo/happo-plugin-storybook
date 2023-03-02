b3fdd15c9c53d5e2f6451a44e59316a3ef7ea6fb
Henric Trotzig <henric.trotzig@gmail.com>
2023-03-02 08:43:42 +0100

    Switch back to disabling framework completely in SB v6 and v5
    I think setting this framework is causing issues down the line.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
+const framework = process.env.DISABLE_REACT_WEBPACK5_FRAMEWORK
+  ? undefined
+  : {
+      name: '@storybook/react-webpack5',
+      options: {},
+    };
+
 module.exports = {
   features: {
     storyStoreV7: process.env.USE_STORYSTORE_V7 !== 'false',
   },
   stories: ['./**/*.stories.js'],
-  addons: process.env.DISABLE_ADDONS
-    ? []
-    : [
-        '@storybook/addon-actions',
-        '@storybook/addon-interactions',
-        '../preset.js',
-      ],
-  framework: {
-    name: process.env.DISABLE_REACT_WEBPACK5_FRAMEWORK
-      ? '@storybook/react'
-      : '@storybook/react-webpack5',
-    options: {},
-  },
+  addons: process.env.DISABLE_ADDONS ? [] : [
+    '@storybook/addon-actions',
+    '@storybook/addon-interactions',
+    '../preset.js',
+  ],
+  framework,
 };
